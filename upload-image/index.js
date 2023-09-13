27179b264ba1c454c9f025784e8072ecf852a6bf
madhav <madhav201singh@gmail.com>
2023-09-13 14:52:01 +0530

    update

--- upload-image/index.js
+++ upload-image/index.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-
-const app = express();
-
-// Middleware to serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Configure multer for handling file uploads
+// Question-5: How to Upload file using Multer....
+// Step-1: Create the server...
+const express =require('express')
+const multer =require('multer')
+const path = require('path')
+const app =express()
+//  Middleware to serve static files from the "public"
+app.use(express.static(path.join(__dirname,'public')))
+// Configure Multer for handling file Uploads...
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+  destination: function (req,file,cb){
+    cb(null,'uploads/');// create the uploads folder...
   },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    cb(null, Date.now() + ext);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// POST request to handle image uploads
-app.post('/upload', upload.single('image'), (req, res) => {
-  if (req.file) {
-    res.json({ message: 'Image uploaded successfully' });
-  } else {
-    res.status(400).json({ error: 'No file uploaded' });
+  filename:function(req,file,cb){
+    const ext = path.extname(file.originalname)
+    cb(null,Date.now() +ext)
   }
-});
+})
+const upload = multer({storage:storage});
+// POST request to handle image uploads...
+app.post('/uploads',upload.single('image'),(req,res)=>{
+  if(req.file){
+    res.json({message:'Image Uploaded Successfully'})
+  }
+  else{
+    res.status(400).json({error:'No file Uploaded'})
+  }
+})
 
-app.listen(8000, () => {
-  console.log('Server is running on port 3000');
-});
+// For Creating the Question we required MULTER and PATH.., import them 
+app.listen(8000,()=>{
+  console.log('Server is Running...')
+})
+// Let's test the Api
+// Thank's for Watching....
