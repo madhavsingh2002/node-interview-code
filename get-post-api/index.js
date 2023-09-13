3d64d2433c9f52082a2453c122186f112e222157
madhav <madhav201singh@gmail.com>
2023-09-13 13:37:15 +0530

    update-get-post

--- get-post-api/index.js
+++ get-post-api/index.js
@@ -1,31 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-// Create an Express.js server that handles both GET and POST requests to a "/api/user" endpoint.
+// Question-4: Create an Express.js server that handles both GET and POST requests to a "/api/user" endpoint.
 // For GET requests, it should respond with a JSON object containing user data (e.g., name and age). 
 // For POST requests, it should accept JSON data representing a new user and add it to a list of users.
-// Middleware to parse JSON data from requests
-app.use(bodyParser.json());
 
-// Initialize an array to store user data
-const users = [];
-
-// GET request to retrieve user data
-app.get('/api/user', (req, res) => {
-  res.json(users);
-});
-
-// POST request to add a new user
-app.post('/api/user', (req, res) => {
+// Step-1: Setup-Server.
+// Step-2: Get Request...
+// Step-3: Post Request...
+// Step-4: use MiddleWare like body-parser.
+// Step-5: Let's test the API
+const express = require('express')
+const bodyParser = require('body-parser')
+const app =express()
+// Middleware to parser JSON data from requests...
+app.use(bodyParser.json())
+// Get Request
+const user =[]// to store the user Data;
+app.get('/api/users',(req,res)=>{
+  res.json(user);
+})
+// Post Request...
+app.post('/api/users',(req,res)=>{
   const newUser = req.body;
-  if (newUser && newUser.name && newUser.age) {
-    users.push(newUser);
-    res.status(201).json(newUser); // Respond with the added user
-  } else {
-    res.status(400).json({ error: 'Invalid user data' });
+  if(newUser && newUser.name && newUser.age){
+    user.push(newUser);
+    res.status(201).json(newUser)
   }
-});
-app.listen(8000,(res,req)=>{
-    console.log('Server is running')
+  else{
+    res.status(400).json({error:'Invalid User Data'})
+  }
+  //else handling
+})
+app.listen(8000,(req,res)=>{
+  console.log('Server is running')
 })
