dbd13c6d928dd25584fa0c57c9b1bbece6b4f4cd
madhav <madhav201singh@gmail.com>
2023-09-15 18:25:52 +0530

    model-find()

--- mongoose-queries/index.js
+++ mongoose-queries/index.js
@@ -9,15 +9,32 @@ const PORT = 8000;
 app.use(bodyParser.json());
 connectToMongodb()
 
-app.post('/users', async (req, res) => {
-    try {
-      const userData=req.body;
-      const newUser = await userModel.create(userData)
-      res.status(201).json(newUser)
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
+// Mongoose Queries-1: Create-> Model.Create()
+// Let's see the example of Model.Create()
+app.post('/users', async (req,res)=>{
+  try{
+    const userData= req.body;
+    const newUser= await userModel.create(userData); // here create is used create new  user ...
+    // let's test this...
+    res.status(201).json(newUser)
+  }
+  catch(err){
+    res.status(501).json({error:err.message})
+  }
+  
+})
+// Mongoose Queries-2: Read-> Model.find()
+// Route to fetch all users
+app.get('/users', async (req, res) => {
+  try {
+    // Use Model.find() to retrieve all users
+    const users = await userModel.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
