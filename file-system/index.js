7057fe9b6feba4dd32ee0741f441d09e677021f6
madhav <madhav201singh@gmail.com>
2023-09-20 10:29:57 +0530

    writeFile

--- file-system/index.js
+++ file-system/index.js
@@ -39,18 +39,35 @@ app.get('/create/appendfile',async(req,res)=>{
 
 // CREATE: fs.open.
 
-app.get('/create/open', (req, res) => {
-    fs.open('newfile.txt', 'w', (err, file) => {
+
+app.get('/create/open',async(req,res)=>{
+    fs.open('newfile2.txt','w',(err,file)=>{
+        if(err){
+            console.error(err)
+            res.status(500).send('Internal server error')
+            return;
+        }
+    })
+    console.log('File Created');
+    res.send('File newfile2.txt created')
+})
+
+// CREATE: fs.writeFile.
+
+app.get('/write', (req, res) => {
+    const contentToWrite = 'Hello content!';
+  
+    fs.writeFile('mynewfile3.txt', contentToWrite, (err) => {
       if (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
         return;
       }
   
-      console.log('File created!');
-      res.send('File mynewfile2.txt created');
+      console.log('File saved!');
+      res.send('File mynewfile3.txt saved');
     });
-});
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
