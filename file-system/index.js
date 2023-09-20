7337201727fc005ae0c1f275719f46b6df7c6efd
madhav <madhav201singh@gmail.com>
2023-09-20 10:04:12 +0530

    create-append

--- file-system/index.js
+++ file-system/index.js
@@ -4,19 +4,40 @@ const fs = require('fs');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  fs.readFile('demo.html', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-  });
+
+// READ: fs.readFile.
+
+app.get('/readfile',async(req,res)=>{
+    fs.readFile('demo.html',(err,data)=>{// here demo.html is file that we want to read.
+        if(err){
+            console.error(err);
+            res.status(500).send('Internal server error')
+            return
+        }
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(data)
+        res.end()
+    })
+})
+
+// CREATE: fs.appendFile.
+
+app.get('/create/appendfile', (req, res) => {
+    const contentToAppend = 'Hello content!';
+  
+    fs.appendFile('mynewfile1.txt', contentToAppend, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+  
+      console.log('Saved!');
+      res.send('Content appended to mynewfile1.txt');
+    });
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
