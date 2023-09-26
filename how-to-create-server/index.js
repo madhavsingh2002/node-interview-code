ea7f0f959797c5699e08a5503de3f65679fe37f7
madhav <madhav201singh@gmail.com>
2023-09-26 22:36:20 +0530

    as

--- how-to-create-server/index.js
+++ how-to-create-server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express();
 // simple route
 app.get('/',(req,res)=>{
-    res.send('Hello World!')
+    res.send('Hello !')
 })
 // Listening server
 app.listen(8000,()=>{
